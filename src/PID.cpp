2ce405d599c4cfefedd1707588599ed5f4ea7c40
vaibhav1284 <vaibhav1284@gmail.com>
2020-07-15 19:05:38 +0530

    add files

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,69 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+#include <algorithm>
+// using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+void PID::Init(double Kp, double Ki, double Kd) {
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
 
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  // Previous cte.
+  prev_cte = 0.0;
+
+  // Counters.
+  counter = 0;
+  errorSum = 0.0;
+  minError = std::numeric_limits<double>::max();
+  maxError = std::numeric_limits<double>::min();
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  // Proportional error.
+  p_error = cte;
 
+  // Integral error.
+  i_error += cte;
+
+  // Diferential error.
+  d_error = cte - prev_cte;
+  prev_cte = cte;
+
+  errorSum += cte;
+  counter++;
+
+  if ( cte > maxError ) {
+    maxError = cte;
+  }
+  if ( cte < minError ) {
+    minError = cte;
+  }
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  return p_error * Kp + i_error * Ki + d_error * Kd;
+}
+
+double PID::AverageError() {
+  return errorSum/counter;
+}
+
+double PID::MinError() {
+  return minError;
+}
+
+double PID::MaxError() {
+  return maxError;
 }
